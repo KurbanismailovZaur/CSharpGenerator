9ff586d6c032098d52cbd89c47c00fa575adfe59
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2022-11-09 01:05:44 +0300

    Added demo scene, test script, readme.pdf.

---
+++ Demo/GeneratedDemoScript.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityRandom = UnityEngine.Random;
+
+namespace MyGame.Modules.Submodules.CSharpGenerator.Demo
+{
+	public class GeneratedDemoScript : MonoBehaviour
+	{
+        private void Start() => print("Script correctly generated!");
+    }
+}
